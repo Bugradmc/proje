214adc45c887ba019cf4617a331a6b8ca4998948
haticedogan069 <haticedoganofficial@gmail.com>
2025-03-02 02:16:32 +0300

    new code

--- script.js
+++ script.js
@@ -3,29 +3,19 @@ function ShopNow(){
 }
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// Artı ikonunu seç
 const addIcon = document.querySelector('.plus-icon');
 const cartCount = document.getElementById('cart-count');
-=======
->>>>>>> 8b6154b132f5387c71afb75d941bc5146b0317fa
 
   const plusIcons = document.querySelectorAll('.plus-icon');
   const cartCount = document.getElementById('cart-count');
 
-<<<<<<< HEAD
-// Artı ikonu tıklanıldığında sepet sayısını artır
+
 addIcon.addEventListener('click', function() {
-  // Eğer sayı 9'dan küçükse, artır
   if (count < 9) {
     count++;
-    cartCount.textContent = count; // Sepet sayısını güncelle
+    cartCount.textContent = count;
   }
 });
->>>>>>> 954808b6697819cfd014cb442914c780e1bee6e3
-=======
 
   plusIcons.forEach(icon => {
     icon.addEventListener('click', () => {
@@ -38,4 +28,3 @@ addIcon.addEventListener('click', function() {
       }
     });
   });
->>>>>>> 8b6154b132f5387c71afb75d941bc5146b0317fa
