954808b6697819cfd014cb442914c780e1bee6e3
Bugradmc <demircanbugrayusuf@gmail.com>
2025-03-02 01:21:57 +0300

    added new codes to the project

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function ShopNow(){
     window.location.href='http://127.0.0.1:5500/product.html';
 }
+
+
+// Artı ikonunu seç
+const addIcon = document.querySelector('.plus-icon');
+const cartCount = document.getElementById('cart-count');
+
+// Başlangıç sayısı
+let count = 0;
+
+// Artı ikonu tıklanıldığında sepet sayısını artır
+addIcon.addEventListener('click', function() {
+  // Eğer sayı 9'dan küçükse, artır
+  if (count < 9) {
+    count++;
+    cartCount.textContent = count; // Sepet sayısını güncelle
+  }
+});
